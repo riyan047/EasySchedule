c1c1ebb06cf09e70c67237a9a77b208b256d5537
Riyan <griyan115@gmail.com>
2025-04-29 22:27:15 +0530

    added dashboard navbar and themes/auth

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -13,10 +13,15 @@ import {
 } from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
+import { ThemeToggle } from "../components/ThemeToggle";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { auth, signOut } from "../lib/auth";
+import { requireUser } from "../lib/hooks";
 
 
 
-export default function DashboardLayout({ children }: { children: ReactNode }) {
+export default async function DashboardLayout({ children }: { children: ReactNode }) {
+    const session = await requireUser()
     return (
         <>
             <div className="min-h-screen w-full grid md:grid-cols-[220px_1fr]
@@ -34,7 +39,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
                         </div>
                         <div className="flex-1">
                             <nav className="grid items-start px-2 lg:px-4">
-                            <DashboardLinks />
+                                <DashboardLinks />
                             </nav>
                         </div>
                     </div>
@@ -44,19 +49,51 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
                         <Sheet>
                             <SheetTrigger asChild>
                                 <Button className="md:hidden shrink-0" size="icon" variant="outline">
-                                    <Menu className="size-5"/>
+                                    <Menu className="size-5" />
                                 </Button>
                             </SheetTrigger>
                             <SheetContent side="left" className="flex flex-col">
                                 <nav className="grid gap-2 mt-10">
-                                    <DashboardLinks/>
+                                    <DashboardLinks />
                                 </nav>
                             </SheetContent>
                         </Sheet>
-                            <div className="ml-auto flex items-center gap-x-4">
-
-                            </div>
+                        <div className="ml-auto flex items-center gap-x-4">
+                            <ThemeToggle />
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <Button variant="secondary" size="icon"
+                                        className="rounded-full"
+                                    >
+                                        <img src={session?.user?.image as string || "/"} alt="Profile Image"
+                                            width={20} height={20} className="w-full h-full rounded-full" />
+                                    </Button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent align="end">
+                                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                                    <DropdownMenuSeparator />
+                                    <DropdownMenuItem asChild>
+                                        <Link href="/dashboard/settings">
+                                            Settings
+                                        </Link>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem asChild>
+                                        <form action={
+                                            async()=>{
+                                                "use server"
+                                                await signOut();
+                                            }
+                                        } className="w-full ">
+                                            <button className="w-full text-left">Logout</button>
+                                        </form>
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </div>
                     </header>
+                    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+                        {children}
+                    </main>
                 </div>
 
             </div>
