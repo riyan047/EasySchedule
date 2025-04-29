b465618290f9131df1e40e5c7e9b1207eee7eeed
Riyan <griyan115@gmail.com>
2025-04-29 20:54:24 +0530

    dashboard/sidebar added

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ReactNode } from "react";
+import Logo from "@/public/logo.png"
+import { DashboardLinks } from "../components/DashboardLinks";
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+
+
+
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+    return (
+        <>
+            <div className="min-h-screen w-full grid md:grid-cols-[220px_1fr]
+                lg:grid-cols-[280px_1fr]">
+                <div className="hidden md:block border-r bg-muted/40">
+                    <div className="flex h-full max-h-screen flex-col gap-2">
+                        <div className="flex h-14 items-center border-b px-4 
+                        lg:h-[60px] lg:px-6">
+                            <Link href="/" className="flex items-center gap-2">
+                                <Image src={Logo} alt="" className="size-8" />
+                                <p className="text-xl font-bold">
+                                    Easy<span className="text-primary">Schedule</span>
+                                </p>
+                            </Link>
+                        </div>
+                        <div className="flex-1">
+                            <nav className="grid items-start px-2 lg:px-4">
+                            <DashboardLinks />
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+                        <Sheet>
+                            <SheetTrigger asChild>
+                                <Button className="md:hidden shrink-0" size="icon" variant="outline">
+                                    <Menu className="size-5"/>
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent side="left" className="flex flex-col">
+                                <nav className="grid gap-2 mt-10">
+                                    <DashboardLinks/>
+                                </nav>
+                            </SheetContent>
+                        </Sheet>
+                            <div className="ml-auto flex items-center gap-x-4">
+
+                            </div>
+                    </header>
+                </div>
+
+            </div>
+        </>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
